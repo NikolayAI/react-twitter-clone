e19711af2e246f44a124c46d41190373238f909f
Nikolay Alexeev <kodi_kodi_kodi@mail.ru>
2021-05-18 15:08:28 +0500

    add tweet options

--- src/components/Tweet/Tweet.tsx
+++ src/components/Tweet/Tweet.tsx
@@ -7,10 +7,12 @@ import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import RepeatIcon from '@material-ui/icons/Repeat';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import ReplyOutlinedIcon from '@material-ui/icons/ReplyOutlined';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Paper from '@material-ui/core/Paper';
 import { useHomeStyles } from '../../pages/Home/homeTheme';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { formatDate } from '../../utils/formatDate';
+import { Menu, MenuItem } from '@material-ui/core';
 
 
 type TweetPropsType = {
@@ -35,9 +37,27 @@ export const Tweet: React.FC<TweetPropsType> = (
     createdAt,
   }
 ) => {
-  console.log(formatDate(new Date(createdAt)));
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const history = useHistory();
+  const open = Boolean(anchorEl);
+
+  const handleClickTweet = (event: React.MouseEvent<HTMLAnchorElement>): void => {
+    event.preventDefault();
+    history.push(`/home/tweet/${_id}`)
+  }
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    event.stopPropagation();
+    event.preventDefault();
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <Link className={classes.tweetWrapper} to={`/home/tweet/${_id}`}>
+    <a className={classes.tweetWrapper} onClick={handleClickTweet} href={`/home/tweet/${_id}`}>
       <Paper
         className={classNames(classes.tweet, classes.tweetsHeader)}
         variant={'outlined'}>
@@ -46,13 +66,41 @@ export const Tweet: React.FC<TweetPropsType> = (
           alt={`Аватарка пользователя ${user.fullname}`}
           src={user.avatarUrl}
         />
-        <div>
-          <Typography>
-            <b>{user.username}</b>&nbsp;
-            <span
-              className={classes.tweetUserName}>@{user.email}</span>&nbsp;
-            <span className={classes.tweetUserName}>&middot;</span>&nbsp;
-            <span className={classes.tweetUserName}>{formatDate(new Date(createdAt))}</span>
+        <div className={classes.tweetContent}>
+          <Typography className={classes.tweetHeader}>
+            <div>
+              <b>{user.username}</b>&nbsp;
+              <span
+                className={classes.tweetUserName}>@{user.email}</span>&nbsp;
+              <span className={classes.tweetUserName}>&middot;</span>&nbsp;
+              <span className={classes.tweetUserName}>
+                {formatDate(new Date(createdAt))}
+              </span>
+            </div>
+            <div>
+              <IconButton
+                aria-label="more"
+                aria-controls="long-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                <MoreVertIcon/>
+              </IconButton>
+              <Menu
+                id="long-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>
+                  Редактировать твит
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  Удалить твит
+                </MenuItem>
+              </Menu>
+            </div>
           </Typography>
           <Typography variant={'body1'} gutterBottom>
             {text}
@@ -81,6 +129,6 @@ export const Tweet: React.FC<TweetPropsType> = (
           </div>
         </div>
       </Paper>
-    </Link>
+    </a>
   );
 };
--- src/pages/Home/homeTheme.ts
+++ src/pages/Home/homeTheme.ts
@@ -1,213 +1,222 @@
-import makeStyles from '@material-ui/core/styles/makeStyles'
-import {Theme} from '@material-ui/core'
-import grey from '@material-ui/core/colors/grey'
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import { Theme } from '@material-ui/core';
+import grey from '@material-ui/core/colors/grey';
+
 
 export const useHomeStyles = makeStyles((theme: Theme) => ({
-    wrapper: {
-        height: '100vh',
-    },
-    logo: {
-        margin: '10px 0',
-    },
-    logoIcon: {
-        fontSize: 36,
-    },
-    sideMenuList: {
-        position: 'sticky',
-        top: 0,
-        listStyle: 'none',
-        padding: 0,
-        margin: 0,
-        maxWidth: 230,
-    },
-    sideMenuListItem: {
-        cursor: 'pointer',
-        '&:hover': {
-            '& div': {
-                backgroundColor: 'rgb(29, 161, 242, 0.1)',
-                '& h6': {
-                    color: theme.palette.primary.main,
-                },
-                '& svg path': {
-                    fill: theme.palette.primary.main,
-                },
-            },
-        },
-        '& div': {
-            display: 'inline-flex',
-            alignItems: 'center',
-            padding: '0 10px',
-            borderRadius: 30,
-            height: 50,
-            marginBottom: 15,
-            transition: 'background-color 0.1s ease-in-out'
-        },
-    },
-    sideMenuListItemLabel: {
-        fontWeight: 700,
-        fontSize: 20,
-        marginLeft: 15,
-    },
-    sideMenuListItemIcon: {
-        fontSize: 28,
-    },
-    sideMenuTweetButton: {
-        padding: theme.spacing(3),
-        marginTop: theme.spacing(2),
-    },
-    tweetsWrapper: {
-        borderRadius: 0,
-        height: '100%',
-        borderTop: 0,
-        borderBottom: 0,
-    },
-    tweetsCentered: {
-        marginTop: 50,
-        textAlign: 'center',
-    },
-    tweetsHeader: {
-        display: 'flex',
-        alignItems: 'center',
-        flex: 1,
-        borderTop: 0,
-        borderLeft: 0,
-        borderRight: 0,
-        borderRadius: 0,
-        padding: '10px 15px',
+  wrapper: {
+    height: '100vh',
+  },
+  logo: {
+    margin: '10px 0',
+  },
+  logoIcon: {
+    fontSize: 36,
+  },
+  sideMenuList: {
+    position: 'sticky',
+    top: 0,
+    listStyle: 'none',
+    padding: 0,
+    margin: 0,
+    maxWidth: 230,
+  },
+  sideMenuListItem: {
+    cursor: 'pointer',
+    '&:hover': {
+      '& div': {
+        backgroundColor: 'rgb(29, 161, 242, 0.1)',
         '& h6': {
-            fontWeight: 800,
+          color: theme.palette.primary.main,
         },
-    },
-    tweetsHeaderUser: {
-        display: 'flex',
-        alignItems: 'center',
-    },
-    tweetsHeaderBackButton: {
-        marginRight: 20,
-    },
-    tweet: {
-        display: 'flex',
-        alignItems: 'flex-start',
-        cursor: 'pointer',
-        paddingTop: 15,
-        '&:hover': {
-            backgroundColor: 'rgb(245, 248, 250)',
+        '& svg path': {
+          fill: theme.palette.primary.main,
         },
+      },
     },
-    tweetWrapper: {
-        color: 'inherit',
-        textDecoration: 'none',
+    '& div': {
+      display: 'inline-flex',
+      alignItems: 'center',
+      padding: '0 10px',
+      borderRadius: 30,
+      height: 50,
+      marginBottom: 15,
+      transition: 'background-color 0.1s ease-in-out'
     },
-    tweetAvatar: {
-        width: theme.spacing(6.5),
-        height: theme.spacing(6.5),
-        marginRight: 15,
+  },
+  sideMenuListItemLabel: {
+    fontWeight: 700,
+    fontSize: 20,
+    marginLeft: 15,
+  },
+  sideMenuListItemIcon: {
+    fontSize: 28,
+  },
+  sideMenuTweetButton: {
+    padding: theme.spacing(3),
+    marginTop: theme.spacing(2),
+  },
+  tweetsWrapper: {
+    borderRadius: 0,
+    height: '100%',
+    borderTop: 0,
+    borderBottom: 0,
+  },
+  tweetsCentered: {
+    marginTop: 50,
+    textAlign: 'center',
+  },
+  tweetsHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    flex: 1,
+    borderTop: 0,
+    borderLeft: 0,
+    borderRight: 0,
+    borderRadius: 0,
+    padding: '10px 15px',
+    '& h6': {
+      fontWeight: 800,
     },
-    tweetFooter: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        position: 'relative',
-        left: -13,
-        maxWidth: 450,
+  },
+  tweetsHeaderUser: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  tweetsHeaderBackButton: {
+    marginRight: 20,
+  },
+  tweet: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    cursor: 'pointer',
+    paddingTop: 15,
+    '&:hover': {
+      backgroundColor: 'rgb(245, 248, 250)',
     },
-    tweetFooterIcon: {
-        fontSize: 20,
+  },
+  tweetWrapper: {
+    color: 'inherit',
+    textDecoration: 'none',
+  },
+  tweetAvatar: {
+    width: theme.spacing(6.5),
+    height: theme.spacing(6.5),
+    marginRight: 15,
+  },
+  tweetHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  tweetContent: {
+    flex: 1,
+  },
+  tweetFooter: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    position: 'relative',
+    left: -13,
+    maxWidth: 450,
+  },
+  tweetFooterIcon: {
+    fontSize: 20,
+  },
+  tweetUserName: {
+    color: grey[500],
+  },
+  fullTweet: {
+    padding: 22,
+  },
+  fullTweetText: {
+    fontSize: 24,
+    marginTop: 20,
+    lineHeight: 1.3125,
+    wordBreak: 'break-word',
+  },
+  rightSide: {
+    paddingTop: 20,
+    position: 'sticky',
+    top: 0,
+  },
+  rightSideBlock: {
+    backgroundColor: '#F5F8FA',
+    borderRadius: 15,
+    marginTop: 20,
+    '& .MuiList-root': {
+      paddingTop: 0,
     },
-    tweetUserName: {
-        color: grey[500],
+  },
+  rightSideBlockHeader: {
+    borderTop: 0,
+    borderLeft: 0,
+    borderRight: 0,
+    backgroundColor: 'transparent',
+    padding: '13px 18px',
+    '& b': {
+      fontSize: 20,
+      fontWeight: 800,
     },
-    fullTweet: {
-        padding: 22,
+  },
+  rightSideBlockItem: {
+    cursor: 'pointer',
+    '& .MuiTypography-body1': {
+      fontWeight: 700,
     },
-    fullTweetText: {
-        fontSize: 24,
-        marginTop: 20,
-        lineHeight: 1.3125,
-        wordBreak: 'break-word',
+    '& .MuiListItemAvatar-root': {
+      minWidth: 50,
     },
-    rightSide: {
-        paddingTop: 20,
-        position: 'sticky',
-        top: 0,
+    '& .MuiListItemItem-root': {
+      margin: 0,
     },
-    rightSideBlock: {
-        backgroundColor: '#F5F8FA',
-        borderRadius: 15,
-        marginTop: 20,
-        '& .MuiList-root': {
-            paddingTop: 0,
-        },
+    '&:hover': {
+      backgroundColor: '#edf3f6',
     },
-    rightSideBlockHeader: {
-        borderTop: 0,
-        borderLeft: 0,
-        borderRight: 0,
-        backgroundColor: 'transparent',
-        padding: '13px 18px',
-        '& b': {
-            fontSize: 20,
-            fontWeight: 800,
-        },
+    '& a': {
+      color: 'inherit',
+      textDecoration: 'none',
     },
-    rightSideBlockItem: {
-        cursor: 'pointer',
-        '& .MuiTypography-body1': {
-            fontWeight: 700,
-        },
-        '& .MuiListItemAvatar-root': {
-            minWidth: 50,
-        },
-        '& .MuiListItemItem-root': {
-            margin: 0,
-        },
-        '&:hover': {
-            backgroundColor: '#edf3f6',
-        },
-        '& a': {
-            color: 'inherit',
-            textDecoration: 'none',
-        },
+  },
+  addForm: {
+    padding: 20,
+  },
+  addFormBody: {
+    display: 'flex',
+    width: '100%',
+  },
+  addFormBottom: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  addFormBottomActions: {
+    marginTop: 10,
+    paddingLeft: 70,
+  },
+  addFormTextarea: {
+    width: '100%',
+    border: 0,
+    fontSize: 20,
+    outline: 'none',
+    fontFamily: 'inherit',
+    resize: 'none',
+  },
+  addFormBottomLine: {
+    height: 12,
+    backgroundColor: '#E6ECF0',
+  },
+  addFormCircleProgress: {
+    position: 'relative',
+    width: 20,
+    height: 20,
+    margin: '0 10px',
+    '& .MuiCircularProgress-root': {
+      position: 'absolute',
     },
-    addForm: {
-        padding: 20,
-    },
-    addFormBody: {
-        display: 'flex',
-        width: '100%',
-    },
-    addFormBottom: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    addFormBottomActions: {
-        marginTop: 10,
-        paddingLeft: 70,
-    },
-    addFormTextarea: {
-        width: '100%',
-        border: 0,
-        fontSize: 20,
-        outline: 'none',
-        fontFamily: 'inherit',
-        resize: 'none',
-    },
-    addFormBottomLine: {
-        height: 12,
-        backgroundColor: '#E6ECF0',
-    },
-    addFormCircleProgress: {
-        position: 'relative',
-        width: 20,
-        height: 20,
-        margin: '0 10px',
-        '& .MuiCircularProgress-root': {
-            position: 'absolute',
-        },
-    },
-    addFormBottomRight: {
-        display: 'flex',
-        alignItems: 'center',
-    },
-    addFormBottomRightSide: {},
-}))
+  },
+  addFormBottomRight: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  addFormBottomRightSide: {},
+}));
